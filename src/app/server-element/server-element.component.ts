60199ef9bd03aba22bf35654b32c459695d6c0f8
Camden Brown <camdenebrown@gmail.com>
2017-08-24 10:52:07 -0500

    using ngOnChanges

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
-  @Input('srvElement') element: { type: string, name: string, content: string };
+export class ServerElementComponent implements OnInit, OnChanges {
+  @Input() name: string;
 
-  constructor() { }
+  constructor() {
+    console.log('constructor called');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!', changes)
+  }
 
   ngOnInit() {
-
+    console.log('ngOnInit called');
   }
 
 }
